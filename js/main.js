88cfd1e5262ec8849f2cd29585df0223a974db84
Yax Dev <dedalboyet16@gmail.com>
2026-07-20 20:38:19 +0800

    psda

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "./vendor/three.module.js";
 
 /* =========================================================
    REAL-TIME FLUID SIMULATION  (Unseen.co–style ink)
